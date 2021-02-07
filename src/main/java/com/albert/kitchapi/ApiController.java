2ffa69eb59d0f2222b6da9bcce92d22f738b8016
albylillington <albylillington@gmail.com>
2021-02-07 14:48:59 +0000

    Allow cross origin requests

--- src/main/java/com/albert/kitchapi/ApiController.java
+++ src/main/java/com/albert/kitchapi/ApiController.java
@@ -1,7 +1,9 @@
 package com.albert.kitchapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @RequestMapping("/api/v1")
 public interface ApiController {
 }
