36c3f018b639edf5ed92264c80b2893b627d6292
albylillington <albylillington@gmail.com>
2021-02-14 10:50:03 +0000

    name refectoring

--- src/main/java/com/albert/kitchapi/Item/ItemApplicationService.java
+++ src/main/java/com/albert/kitchapi/Item/ItemApplicationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ItemServiceImpl implements ItemService {
+public class ItemApplicationService implements ItemService {
 
     @Autowired
     ItemRepository itemRepository;
--- src/main/java/com/albert/kitchapi/User/UserApplicationService.java
+++ src/main/java/com/albert/kitchapi/User/UserApplicationService.java
@@ -3,6 +3,6 @@ package com.albert.kitchapi.User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserApplicationService implements UserService {
 
 }
